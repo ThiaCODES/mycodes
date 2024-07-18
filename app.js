e48aaadc408e54c114071cb521f8de93342c39c8
ThiaCODES <moshoodfatia@gmail.com>
2024-07-18 04:20:09 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//alert('Hello world')
+console.log('hello')
+console.table({name:'fat', sex: 2})
+console.error('its dangerous')
+console.warn(stop)
+console.log(2,3,4,5)
+console.time('ola')
+console.table({name:'fat', sex: 2})
+console.error('its dangerous')
+console.warn(stop)
+console.log(2,3,4,5)
+console.timeEnd('ola')
+//console.clear()
+/*console.info('i love you')
+let arr=[ 1,2,3,4,5,6]
+ let bim= arr.slice(-5);
+ alert(bim)*/

a8f16811731d15e4193d4a734235d0097c4ec2f8
ThiaCODES <moshoodfatia@gmail.com>
2024-07-18 04:34:30 -0700

    changed app.js

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ console.error('its dangerous')
 console.warn(stop)
 console.log(2,3,4,5)
 console.timeEnd('ola')
-//console.clear()
-/*console.info('i love you')
+console.clear()
+console.info('i love you')
 let arr=[ 1,2,3,4,5,6]
  let bim= arr.slice(-5);
- alert(bim)*/
+ alert(bim)
+ console.log ('hello world')

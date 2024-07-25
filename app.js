85a9e679459d9f4d37f764817f1b4204125d8411
ThiaCODES <moshoodfatia@gmail.com>
2024-07-25 19:13:22 +0100

    add commit

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ console. log(Array.isArray(w))
 console. log(typeof w)
 console.log(w.length)
 //two
+/*
 const name = "fathiat";
 const school = "uniosun";
 const age = 22
@@ -88,7 +89,9 @@ mixed.splice(4,5);
 mixed.reverse()
 // concat arrays join
 val = mixed. concat(zee)
-//sorting method using function to sort out perfectly
+//sorting method using 
+
+function to sort out perfectly
 zee.sort(function (x,y,) {
     return x-y
 })
@@ -98,7 +101,138 @@ console.log(mixed)
 console.log(val)
 console.log(zee)
 
+//date and time 
+let today;
+day = new Date()
+today= day. getMonth()
+today= day.getHours()
+today = day.getMilliseconds()
+today= day.getMinutes()
+today = day.getDay()
+today = day.getDate()
+today= day.setFullYear(2002)
+
+console.log(day);*/
+
+let be;
+be = 30
+if(be===100){
+    console.log("correct");
+    document.body.innerHTML= "correct"
+}else{
+    console.log ("incorrect");
+    document.body.innerHTML= `This is incorrect`
+}
+if (be < 18 && be > 60){
+    console.log ('not qualify')
+} else if ( be < 5) {
+    console.log('now')
+}else{
+    console.log("before")
+}
+let colour = 'red';
+switch (colour) {
+    case "red":
+    console.log('colour is red')
+        break;
+    case "blue":
+     console.log('colour is blue')
+            break;
+    
+    default:
+        break;
+}
+let day;
+switch(new Date(). getDay()){
+    case 0: 
+    day= "sunday";
+    break;
+    case 1: 
+    day= "monday";
+    break;
+    case 2: 
+    day= "tuesday";
+    break;
+    case 3: 
+    day= "wednesday";
+    break;
+    case 4: 
+    day= "thursday";
+    break;
+    case 5: 
+    day= "friday";
+    break;
+    case 6: 
+    day= "saturday";
+    break;
+    default: "No day"
+break;
+}
+document.body.innerHTML= ` Today's date is ${day}`;
+console.log(day);
+
+
+//function statement, a block of declaration
+function value (a,b){
+    return a-b;
+    
+}
+console.log(value(2,5));
+
+function calc (hello){
+hello = 5
+//console.log(typeof hello)
+if (typeof hello === Number) {
+    console.log('is true')
+}else{
+    console.log("wrong");
+}
+}
+calc()
+//immediately invoked function expression (iife)
+/*let object= (function (hello) {
+     hello = "Hi"
+    return`${hello} ma`
+})
+console.log (object())
+
+let ob = {
+    ola: function (a,b) {
+        return a % b
+    }
+}
+console.log (ob.ola(11,10))
 
 
 
- 
+
+let obe = { ike : function (he) {
+    return 'ola'
+}}
+console.log(obe.ike())
+*/
+
+
+//loops for loops
+const names = ['faizah', 'fathiat', 'faridah', 'faruq', 'idayat', 'kazeem']
+/*for (i =0; i < names.length; i++) {
+    console.log( `${i} ${names[i]}`)
+}*/
+
+//using for each method
+/*names.forEach(function(name,index,array){
+    console.log(`${index} ${name}`)
+        console.log (`${array}`)
+
+}
+)*/
+//using map method
+let ink = [
+    {id: 3, be : 2},
+    {id: 35, be : 20},
+    {id: 30, be : 29}
+]
+const c = ink.map(function (biro){
+return biro.id
+})
+console.log(c)
